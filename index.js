5e96f9ddef5f6f17e26f2418a339a1541e94c3e0
Devesh Jangid <deveshjangid669@gmail.com>
2023-06-28 01:47:30 +0530

    -- stopwatch finished --

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var isStop = true;
+var ms = 0;
 var s = 0;
 var min = 0;
 var hr = 0;
@@ -12,17 +13,25 @@ function start(){
 }
 function timer(){
     if(isStop==false){
+        ms=parseInt(ms);
         s=parseInt(s);
         min=parseInt(min);
         hr=parseInt(hr);
-        s++;
-        if(s==60){
+        ms++;
+        if(ms===99){
+            ms=0;
+            s++;
+        }
+        if(s===59){
             s=0;
             min++;
         }
-        if(min==60){
+        if(min===59){
             min=0;
             hr++;
+        }
+        if(ms<10){
+            ms="0"+ms;
         }
         if(s<10){
             s="0" +s;
@@ -39,16 +48,17 @@ function timer(){
 
 
         // console.log(s);
-        stopwatch.innerHTML = hr +" : "+min+" : "+s;
-        setTimeout("timer()",1000);
+        stopwatch.innerHTML = hr +" : "+min+" : "+ s + " : "+ ms;
+        setTimeout("timer()",10);
     }
 }
 function stop(){
     isStop=true; 
 }
 function reset(){
+    ms=0;
     s=0;
     min=0
     hr=0;
-    stopwatch.innerHTML="00 : 00 : 00";
+    stopwatch.innerHTML="00 : 00 : 00 : 00";
 }
